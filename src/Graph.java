997e3c3f985948d13f98526c766a93c67be57503
Anna Jang <aj3spencer@gmail.com>
2026-03-27 15:03:45 -0700

    Added Graph class + initial method descriptions

---
+++ src/Graph.java
@@ -0,0 +1,157 @@
+/**
+ *
+ * @author Anna Jang
+ * @set C
+ * @studentnumber A01405877
+ */
+public class Graph
+{
+
+    /**
+     * Graph constructor for an NxN adjacency matrix and initialized to all zeroes.
+     * @param vertexLabels an array of Strings that represent the names of vertices.
+     *                     The number of names will determine N, the size of the graph.
+     * @param isDirected whether the graph is directed or undirected.
+     */
+    public Graph(String[] vertexLabels, boolean isDirected)
+    {
+
+    }
+
+    /**
+     * Returns whether the graph is directed.
+     * @return true or false depending on if the graph is directed or undirected respectfully.
+     */
+    public boolean isDirected()
+    {
+
+    }
+
+    /**
+     * Adds an edge between vertex A to vertex B.
+     * If the graph is undirected, both entries in the adjacency matrix set to 1.
+     * If any string is not present in the list of vertices, the function quietly does nothing.
+     * @param a label of vertex A.
+     * @param b label of vertex B.
+     */
+    public void addEdge(String a, String b)
+    {
+
+    }
+
+    /**
+     * Returns the number of vertices in the adjacency matrix.
+     * @return number of vertices in the adjacency matrix.
+     */
+    public int size()
+    {
+
+    }
+
+    /**
+     * Returns the string name of the vertex numbered v in the list of vertices.
+     * @param v assumed to be an integer in range 0->N-1 inside the adjacency matrix.
+     * @return the name of the vertex numbered 'v' in the vertices list.
+     */
+    public String getLabel(int v)
+    {
+
+    }
+
+    /**
+     * Returns a string representation of the adjacency matrix.
+     * Includes one line of "output" for each vertex in the same order they are listed in the graph.
+     * @return a human-readable string of the adjacency matrix.
+     */
+    public String toString()
+    {
+
+    }
+
+    /**
+     * Performs DFS algorithm on the graph. The first vertex will be the default starting position.
+     *
+     * The output resulting from this method should include all the vertices of the graph even if the graph is not connected
+     * @param quiet if true, the console will produce no output.
+     *              If false, the method will print out the sequence of vertices as they are visited.
+     */
+    public void runDFS(boolean quiet)
+    {
+
+    }
+
+    /**
+     * Performs DFS algorithm on the graph, starting at vertex v.
+     * As the program runs, save two lists of vertices internally.
+     * The first will be the DFS order of the vertices which is the order they are visited during the search.
+     * The second list is the finished order (aka dead-end-order)
+     *
+     * If the graph is not connected, the output of this method will not include all the vertices of the graph,
+     * but only the ones reachable from the start vertex v
+     * @param v specified vertex to start from.
+     * @param quiet if true, the console will produce no output.
+     *              If false, the method will print out the sequence of vertices as they are visited.
+     */
+    public void runDFS(String v, boolean quiet)
+    {
+
+    }
+
+    /**
+     * Runs BFS algorithm on the graph. The first vertex will be the default starting position.
+     *
+     * The output resulting from this method will include all the vertices of the graph even if the graph is not connected
+     * @param quiet if true, the console will produce no output.
+     *              If false, the method will print out the sequence of vertices as they are visited.
+     */
+    public void runBFS(boolean quiet)
+    {
+
+    }
+
+    /**
+     * Runs BFS algorithm on the graph, starting at vertex v
+     * As the program runs, a list of vertices will be saved in the order they are visited during the search.
+     *
+     * If the graph is not connected, the output of this method will not include all the vertices of the graph,
+     * but only the ones reachable from the start vertex v
+     * @param v specified vertex to start from.
+     * @param quiet if true, the console will produce no output.
+     *              If false, the method will print out the sequence of vertices as they are visited.
+     */
+    public void runBFS(String v, boolean quiet)
+    {
+
+    }
+
+    /**
+     * Gets the result of the most recently performed DFS.
+     * @return a string containing the DFS order results of the most recently performed DFS.
+     *         If no RunDFS() method has been called, a message will be displayed to the user.
+     */
+    public String getLastDFSOrder()
+    {
+
+    }
+
+    /**
+     * Returns a string containing the dead-end order results of the most recently performed DFS.
+     * @return a string containing the dead-end order result of the most recently performed DFS.
+     *         If no RunDFS() method has been called, a message will be displayed to the user.
+     */
+    public String getLastDFSDeadEndOrder()
+    {
+
+    }
+
+    /**
+     * Get results of most recently performed BFS.
+     * @return a string containing the result of the most recently performed BFS.
+     *         if no RunBFS() was called, a message will be displayed to the user.
+     */
+    String getLastBFSOrder()
+    {
+
+    }
+
+}
